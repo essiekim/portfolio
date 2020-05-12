daada8083bb0f6cf93377ad4e22a7f30c0ee1605
Essie <esther.ys.kim@gmail.com>
2020-05-12 13:44:32 -0400

    forgot something oops pt 2

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteTitle: 'Essie Kim / Developer', // <title>
-  manifestName: 'Spectral',
-  manifestShortName: 'Landing', // max 12 characters
+  manifestName: 'Essie Kim / Developer',
+  manifestShortName: 'Essie Kim', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/essiekim/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'essie kim.',
   subHeading: 'full stack developer / graphic designer',
   // project info
